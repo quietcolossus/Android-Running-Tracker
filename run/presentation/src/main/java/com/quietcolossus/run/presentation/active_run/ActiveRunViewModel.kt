1a60017e8c027670adbacb01c8c56f6ebc3ca378
Colin <colinm@rspndr.io>
2024-09-11 14:25:30 -0400

    feat: add foreground service to maintain runs when app closed

--- run/presentation/src/main/java/com/quietcolossus/run/presentation/active_run/ActiveRunViewModel.kt
+++ run/presentation/src/main/java/com/quietcolossus/run/presentation/active_run/ActiveRunViewModel.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.quietcolossus.run.domain.RunningTracker
+import com.quietcolossus.run.presentation.active_run.service.ActiveRunService
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
@@ -22,7 +23,10 @@ class ActiveRunViewModel(
     private val runningTracker: RunningTracker
 ): ViewModel() {
 
-    var state by mutableStateOf(ActiveRunState())
+    var state by mutableStateOf(ActiveRunState(
+        shouldTrack = ActiveRunService.isServiceActive && runningTracker.isTracking.value,
+        hasStartedRunning = ActiveRunService.isServiceActive
+    ))
         private set
 
     private val eventChannel = Channel<ActiveRunEvent>()
@@ -101,7 +105,12 @@ class ActiveRunViewModel(
             }
             else -> Unit
         }
-
     }
 
+    override fun onCleared() {
+        super.onCleared()
+        if(!ActiveRunService.isServiceActive) {
+            runningTracker.stopObservingLocation()
+        }
+    }
 }
